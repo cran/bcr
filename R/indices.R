c660cfcd74098b1bc8105b1448fda2d47ddb3218
Abdessabour Moutik <a.moutik@insea.ac.ma>
2020-11-23 08:10:05 +0000

    version 0.1.0

---
+++ R/indices.R
@@ -0,0 +1,21 @@
+
+#' Moroccan Market and Sector indices
+#' @description 
+#' Gets the Moroccan Market and Sector indices that are listed in [**Le Boursier**](http://leboursier.ma/details/marche.html).
+#' @export
+#' @rdname indices
+#' @examples
+#' \dontrun{
+#'   indices()}
+indices <- function() setNames(do.call(rbind, lapply(1:5, function(x) cbind(type=index_types[x], get_html(paste0("http://leboursier.ma/index.php?option=com_api&view=api&method=indexInfoByType&format=json&type=", x))))), nm=c("type", "name", "last_date", "price", "open", "close", "variation", "low", "high"))
+
+
+
+
+
+#' @export
+#' @rdname indices
+#' @examples
+#' \dontrun{
+#'   sector_indices()}
+sector_indices <- function() setNames(get_html("http://leboursier.ma/index.php?option=com_api&view=api&method=sectorsInfo&format=json"),nm=c("name", "price", "variation"))
